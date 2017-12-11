e3e4b825080fb12231c1adc21c0299a71cfaabba
Unknown <francima93@gmail.com>
2017-12-11 13:02:12 +0100

    TEst

--- Rete.py
+++ Rete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import division
 import nest
 import nest.voltage_trace
 import nest.raster_plot
@@ -29,7 +30,8 @@ V_th_Granular = -41.0
 V_th_Golgi = -55.0     # [mV]
 N_Granular = 20000     # [Sahasranamam et al, NatSciRep, 2016]; all RS
 N_Golgi = 45
-N_Glomeruli= 1500  
+N_Glomeruli= 1500
+N_Mossy = 75  
 
 N_neurons = N_Golgi+N_Granular+N_Glomeruli
 
@@ -47,7 +49,7 @@ J_E = 0.1       # Peak of alpha function for synapses
 J_I = -g*J_E
 #nu_ex = eta*V_th/(J_E*C_E)
 #p_rate = 1000.0*nu_ex*C_E      # From spk/ms to spk/s
-p_rate = 6.0 #perchè la frequenza di stimolazione è 6Hz
+p_rate = 2.0/N_Mossy #perchè la frequenza di stimolazione è 6Hz
 #------------------------------------------------------
  
  
@@ -61,10 +63,20 @@ neu = nest.Create('izhikevich', (N_neurons-N_Glomeruli))
 neu_Granular = neu[:N_Granular]
 neu_Golgi = neu[N_Granular:]
 
- 
-#nest.SetStatus(neu_Granular, {'a': 0.02,'b': 0.2,'c': -65.0, 'd': 8.0,'V_th': V_th_Granular,})
-nest.SetStatus(neu_Granular, {'a': 0.1,'b': 0.2,'c': -65.0, 'd': 2.0,'V_th': V_th_Granular})
-nest.SetStatus(neu_Golgi, {'a': 0.1,'b': 0.2,'c': -65.0, 'd': 2.0,'V_th': V_th_Golgi})
+
+# Random
+#nest.SetStatus(neu_Granular, {'a': 0.1,'b': 0.2,'c': -65.0, 'd': 2.0,'V_th': V_th_Granular})
+#nest.SetStatus(neu_Golgi, {'a': 0.1,'b': 0.2,'c': -65.0, 'd': 2.0,'V_th': V_th_Golgi})
+
+# Bursting
+#nest.SetStatus(neu_Golgi, {'a': 0.02,'b': 0.2,'c': -75.0, 'd': 2.0,'V_th': V_th_Golgi})
+#nest.SetStatus(neu_Granular, {'a': 0.02,'b': 0.2,'c': -70.0, 'd': 2.0,'V_th': V_th_Granular,})
+
+# La b è il reciproco della tau di membrana, 0.17 Granular, 0.023 Golgi per similarità con LEAK INTEGRATE AND FIRE
+nest.SetStatus(neu_Golgi, {'a': 0.02,'b': 0.17,'c': -75.0, 'd': 2.0,'V_th': V_th_Golgi,'I_e':3.0})
+nest.SetStatus(neu_Granular, {'a': 0.02,'b': 0.023,'c': -70.0, 'd': 2.0,'V_th': V_th_Granular,'I_e':50.0})
+
+
 #da mettere a variare i diversi input, mentre rimane invariato il fatto di avere bursting o spike o altro.
 #per i valori che possiamo recuperare da fisiologia li mettiamo fissi (con il valore pari al valore della grandezza con le stess eunità di misura)
  
@@ -74,14 +86,23 @@ neu_Glomeruli=nest.Create('parrot_neuron',N_Glomeruli)
 neu_TOT = neu_Glomeruli+neu
  
 # Neuroni esterni
-mossy = nest.Create('poisson_generator',75,{'rate': p_rate}) #il p-rate va bene che sia 6, da controllare (da variare nel range)
+mossy = nest.Create('poisson_generator',N_Mossy,{'rate': p_rate}) #il p-rate va bene che sia 6, da controllare (da variare nel range)
 #le mossy fibers le modellizziamo come un generatore di poisson (quindi le mossy sono poisson)
  
 #Spike detector
-spk = nest.Create('spike_detector', 51, params = {"to_file": True,
+spk_Granular = nest.Create('spike_detector', 1, params = {"to_file": True,
             "withgid": True,
-            "label": "spikes"})
- 
+            "label": "Spikes_Granular"})
+
+spk_Golgi = nest.Create('spike_detector', 1, params = {"to_file": True,
+            "withgid": True,
+            "label": "Spikes_Golgi"})
+
+spk_Glomeruli = nest.Create('spike_detector', 1, params = {"to_file": True,
+            "withgid": True,
+            "label": "Spikes_Glomeruli"})
+
+
 #Multimeters
 m_Golgi = nest.Create("multimeter",
                 params = {"interval": 1.0,
@@ -109,16 +130,86 @@ nest.Connect(neu_Glomeruli,neu_Granular,{'rule':'fixed_indegree','indegree': C_G
 nest.Connect(neu_Golgi,neu_Granular,{'rule':'fixed_indegree','indegree': C_Gol_Gra},'inhibitory')
 nest.Connect(neu_Golgi,neu_Golgi,{'rule':'fixed_indegree','indegree': C_Gol_Gol},'inhibitory')
 nest.Connect(neu_Glomeruli,neu_Golgi,{'rule':'fixed_indegree','indegree': C_Glo_Gol},'excitatory')
-nest.Connect(neu_Granular,spk)
+nest.Connect(neu_Glomeruli,spk_Glomeruli)
+nest.Connect(neu_Golgi,spk_Golgi)
+nest.Connect(neu_Granular,spk_Granular)
 nest.Connect(m_Golgi,neu_Golgi)
 nest.Connect(m_Granular,neu_Granular)
 
-#nest.Connect(spk,m)
- 
-nest.Simulate(600.0)
-try: nest.raster_plot.from_device(spk, hist=True)
+
+nest.Simulate(500.0)
+
+pl.figure()
+
+try: nest.raster_plot.from_device(spk_Glomeruli, hist=True)
 except nest.NESTError as err:
 	print err
 
+try: nest.raster_plot.from_device(spk_Golgi, hist=True)
+except nest.NESTError as err:
+	print err
+
+try: nest.raster_plot.from_device(spk_Granular, hist=True)
+except nest.NESTError as err:
+	print err
+
+events = nest.GetStatus(m_Granular)[0]['events']
+t = events['times'];
+pl.subplot(211)
+pl.plot(t, events['V_m'])
+pl.axis([0, 100, -100, -50])
+pl.ylabel('Membrane potential [mV]')
+
+pl.show()
+
+
+
 #rasterplot alla fine di ogni simulazione, come input poisson sia segnale che rumore. usiamo un poisson per tutti i neuroni a diverse frequenze.
 #siamo felici quando dando un input a una frequenza tra 8-10 Hz le granular hanno andamento oscialltorio a 6 Hz di risonanza.
+
+
+
+"""
+
+- Fissare parametro corrente in modo tale che le granular con input minimo producano degli spike (2Hz di popolazione).
+- Dare diversi valori agli altri parametri in modo da differenziare in bursting/fast spiking oppure regular/bursting golgi e granular
+    - Prima mettere il bursting solo alle golgi, poi le altri fast o regular. 
+
+- Fare simulazione con le tre frequenze, magari cambiare anche i pesi. 
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
